bc3fa46cf235ac2dc2e28077eb88b7cd3aac98f5
Nicholas Limparis <nicholas@github.limpar.is>
2020-01-20 01:16:48 -0500

    Added the file handeling for the first data type output file so that is asks if you want to overwite a file that already exists

--- src/ADCSledRaw2Dat.py
+++ src/ADCSledRaw2Dat.py
@@ -62,18 +62,14 @@ def main():
     
     #Check to see if the first command line argument is a file
     if not os.path.isfile(str(sys.argv[1])): 
-        print("not a file")
         sys.exit("The input file given is not a file")   
     else:
-        print("Am a file")
         inputFileName = str(sys.argv[1])
         
     #Check to see if the second command line argument is a directory
     if (not os.path.isdir(str(sys.argv[2]))):
-        print("not dir")
         sys.exit("The output file path is not a valid directory or does not exist")
     else:
-        print("is dir")
         outputDirectory = str(sys.argv[2])
 
     #At this point we can assume the file is good and the save directory exists.
@@ -85,10 +81,28 @@ def main():
     #seperate the file into individual lines
     inputLines  = inFile.readlines()
 
-    #the first 4 lines are human readable garbage and need to be tossed
+    # The first 4 lines are human readable garbage and need to be tossed. The first 
+    # line with real data is line index 4
 
-    print(inputLines[4])
-    print(outputDirectory)
+    #print(inputLines[3])
+    #print(outputDirectory)
+
+    # Create the 3 output files for the different data types
+    
+    # Check to see if the file exists
+    if os.path.isfile(outputDirectory+"ADCCurrentReadings.dat"):
+        print("The output file for the current sensor data exist. Overwrite? [y/n]")
+        userInput = input()
+        if userInput[0] == "y" or userInput[0] == "Y":
+            os.remove(outputDirectory+"ADCCurrentReadings.dat")
+        else:
+            sys.exit("Program exiting so as to not overwrite the file")
+
+    # Open file for output failing nicely if it already exists
+    outFileCurrent = open(outputDirectory+"ADCCurrentReadings.dat","x")
+
+    
+
 
     # This allows the file to run as stand alone or as called to run by another program.
 if __name__ == "__main__":
